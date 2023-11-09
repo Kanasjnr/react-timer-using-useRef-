5c35e6908e8a8f67ac6f8f18c899d91bbc940090
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-11-09 11:43:46 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState, useEffect, useCallback, useMemo } from "react";
+import { useState, useEffect } from "react";
 
-const getArray = () => {
-  for (let i = 0; i < 1000000000; i++) {}
-  return ["Muhammed", "Fine Boy"];
-};
+
 
 const App = () => {
   const [userInput, setUserInput] = useState("");
   const [randomInput, setRandomInput] = useState("");
 
-  const fib = useCallback((n) => {
-    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
-  }, []);
+ 
 
   const fibNumber = useMemo(() => fib(userInput), [userInput, fib]);
 
